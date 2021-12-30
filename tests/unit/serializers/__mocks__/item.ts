ceee78663adb49c0a46a78316d2248c9108c5d1f
raytruong <raylintruong@gmail.com>
2021-12-29 20:32:32 -0500

    Fix itemSerializer unit tests

--- tests/unit/serializers/__mocks__/item.ts
+++ tests/unit/serializers/__mocks__/item.ts
@@ -1,41 +1,133 @@
 import Addon from '@/lib/models/Addon';
 import Item from '@/lib/models/Item';
-import Currency from '../../../../src/lib/models/Currency';
+import Currency from '@/lib/models/Currency';
+
+export const addonAJSON = {
+    _id: 'addon:1234567890',
+    _rev: '7-8596f70bd9ed85a3e133af283838f191',
+    name: 'Gel Powder',
+    price: new Currency(100),
+    quantity: 1,
+    category: 'Addon',
+};
 
 export const addonA = new Addon(
-    'addon:1234567890',
-    '7-8596f70bd9ed85a3e133af283838f191',
-    'Gel Powder',
-    new Currency(100),
-    1,
-    'Addon',
+    addonAJSON._id,
+    addonAJSON._rev,
+    addonAJSON.name,
+    addonAJSON.price,
+    addonAJSON.quantity,
+    addonAJSON.category,
 );
+
+export const addonBJSON = {
+    _id: 'addon:0987654321',
+    _rev: '3-8596f70bd9ed85a3e133af283838f191',
+    name: 'Dipping Powder',
+    price: new Currency(100),
+    quantity: 2,
+    category: 'Addon',
+};
 
 export const addonB = new Addon(
-    'addon:0987654321',
-    '7-8596f70bd9ed85a3e133af283838f191',
-    'Dipping Powder',
-    new Currency(100),
-    2,
-    'Addon',
+    addonBJSON._id,
+    addonBJSON._rev,
+    addonBJSON.name,
+    addonBJSON.price,
+    addonBJSON.quantity,
+    addonBJSON.category,
 );
 
-export const mockItemA = new Item(
-    'item:1234567890',
-    '7-8596f70bd9ed85a3e133af283838f191',
-    'Classic Pedicure',
-    new Currency(100),
-    1,
-    'Pedicure',
-    new Array<Addon>(addonA, addonB),
+export const itemAJSON = {
+    _id: 'item:1234567890',
+    _rev: '7-8596f70bd9ed85a3e133af283838f191',
+    name: 'Classic Pedicure',
+    price: new Currency(100),
+    quantity: 1,
+    category: 'Pedicure',
+    addons: new Array<Addon>(addonA, addonB),
+};
+
+export const itemA = new Item(
+    itemAJSON._id,
+    itemAJSON._rev,
+    itemAJSON.name,
+    itemAJSON.price,
+    itemAJSON.quantity,
+    itemAJSON.category,
+    itemAJSON.addons,
 );
 
-export const mockItemB = new Item(
-    'item:0987654321',
-    '1-5782E71F1E4BF698FA3793D9D5A96393',
-    'Kids Pedicure',
-    new Currency(300),
-    2,
-    'Pedicure',
-    new Array<Addon>(addonA, addonB),
+export const itemBJSON = {
+    _id: 'item:0987654321',
+    _rev: '1-5782E71F1E4BF698FA3793D9D5A96393',
+    name: 'Kids Pedicure',
+    price: new Currency(300),
+    quantity: 2,
+    category: 'Pedicure',
+    addons: new Array<Addon>(addonA, addonB),
+};
+
+export const itemB = new Item(
+    itemBJSON._id,
+    itemBJSON._rev,
+    itemBJSON.name,
+    itemBJSON.price,
+    itemBJSON.quantity,
+    itemBJSON.category,
+    itemBJSON.addons,
 );
+
+export const SerializedItemA = {
+    _id: itemAJSON._id,
+    _rev: itemAJSON._rev,
+    name: itemAJSON.name,
+    price: itemAJSON.price.toCents(),
+    quantity: itemAJSON.quantity,
+    category: itemAJSON.category,
+    addons: [
+        {
+            _id: addonAJSON._id,
+            _rev: addonAJSON._rev,
+            name: addonAJSON.name,
+            price: addonAJSON.price.toCents(),
+            quantity: addonAJSON.quantity,
+            category: addonAJSON.category,
+        },
+        {
+            _id: addonBJSON._id,
+            _rev: addonBJSON._rev,
+            name: addonBJSON.name,
+            price: addonBJSON.price.toCents(),
+            quantity: addonBJSON.quantity,
+            category: addonBJSON.category,
+        },
+    ],
+};
+
+export const SerializedItemB = {
+    _id: itemBJSON._id,
+    _rev: itemBJSON._rev,
+    name: itemBJSON.name,
+    price: itemBJSON.price.toCents(),
+    quantity: itemBJSON.quantity,
+    category: itemBJSON.category,
+    addons: [
+        {
+            _id: addonAJSON._id,
+            _rev: addonAJSON._rev,
+            name: addonAJSON.name,
+            price: addonAJSON.price.toCents(),
+            quantity: addonAJSON.quantity,
+            category: addonAJSON.category,
+        },
+        {
+            _id: addonBJSON._id,
+            _rev: addonBJSON._rev,
+            name: addonBJSON.name,
+            price: addonBJSON.price.toCents(),
+            quantity: addonBJSON.quantity,
+            category: addonBJSON.category,
+        },
+    ],
+};
--- tests/unit/serializers/itemSerializer.spec.ts
+++ tests/unit/serializers/itemSerializer.spec.ts
@@ -1,16 +1,19 @@
 import { container } from 'tsyringe';
-import ItemSerializer from '../../../src/lib/serializers/ItemSerializer';
-import { mockItemA, mockItemB } from './__mocks__/item';
-import Item from '../../../src/lib/models/Item';
+import ItemSerializer from '@/lib/serializers/ItemSerializer';
+import Item from '@/lib/models/Item';
+import {
+    itemA,
+    itemB,
+    SerializedItemA,
+    SerializedItemB,
+} from './__mocks__/item';
 
 describe('ItemSerializer class', () => {
     const mockContainer = container.createChildContainer();
     let serializer: ItemSerializer;
-    let mockItem = mockItemA;
 
     beforeEach(() => {
         serializer = mockContainer.resolve(ItemSerializer);
-        mockItem = mockItemA;
     });
 
     afterEach(() => {
@@ -19,21 +22,37 @@ describe('ItemSerializer class', () => {
 
     it('should create an ItemSerializer', () => {
         expect(serializer).toBeDefined();
+        expect(serializer).toBeInstanceOf(ItemSerializer);
     });
 
     it('should serialize an Item with addons', async () => {
-        const expected = serializer.deserialize(serializer.serialize(mockItem));
-        const actual = serializer.serialize(mockItem);
+        const expected = SerializedItemA;
+        const actual = serializer.serialize(itemA);
         expect(actual).toBeInstanceOf(Object);
+        expect(actual).toStrictEqual(expected);
     });
 
-    it('should serialize multiple Items with addons', () => {});
+    it('should serialize multiple Items with addons', () => {
+        const expected = [SerializedItemA, SerializedItemB];
+        const actual = serializer.serializeAll([itemA, itemB]);
+        expect(actual).toBeInstanceOf(Array);
+        expect(actual).toStrictEqual(expected);
+    });
 
     it('should deserialize an Item with addons', async () => {
-        const expected = mockItem;
-        const serialized = serializer.serialize(mockItem);
-        const actual = serializer.deserialize(serialized);
+        const expected = itemA;
+        const actual = serializer.deserialize(SerializedItemA);
         expect(actual).toBeInstanceOf(Item);
+        expect(actual).toStrictEqual(expected);
+    });
+
+    it('should deserialize multiple Items with addons', async () => {
+        const expected = [itemA, itemB];
+        const actual = serializer.deserializeAll([
+            SerializedItemA,
+            SerializedItemB,
+        ]);
+        expect(actual).toBeInstanceOf(Array);
         expect(actual).toStrictEqual(expected);
     });
 
@@ -41,11 +60,9 @@ describe('ItemSerializer class', () => {
         expect(
             serializer.deserialize(
                 serializer.serialize(
-                    serializer.deserialize(serializer.serialize(mockItem)),
+                    serializer.deserialize(serializer.serialize(itemA)),
                 ),
             ),
-        ).toStrictEqual(mockItem);
+        ).toStrictEqual(itemA);
     });
-
-    it('should deserialize multiple Items with addons', async () => {});
 });
