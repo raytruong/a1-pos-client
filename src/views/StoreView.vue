037b707b5913436b6db9802e175ac6616a341bc8
raytruong <raylintruong@gmail.com>
2022-01-22 10:32:42 -0600

    Fix linting

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -2,68 +2,69 @@
     <TwoPanelLayout>
         <template #leftPanel>
             <div
-                class="
+                class='
                     grid grid-rows-12 grid-cols-1
                     h-screen
                     bg-gray-50
                     px-4
                     py-6
-                "
+                '
             >
-                <div class="px-8 row-span-1 flex justify-between items-center">
-                    <h1 class="text-4xl">A1 Nails Salon</h1>
+                <div class='px-8 row-span-1 flex justify-between items-center'>
+                    <h1 class='text-4xl'>A1 Nails Salon</h1>
                     <HealthIndicator />
                 </div>
-                <div class="row-span-1 flex items-center px-3">
+                <div class='row-span-1 flex items-center px-3'>
                     <RadioGroup></RadioGroup>
                 </div>
-                <div class="row-span-8 flex items-center px-6">
+                <div class='row-span-8 flex items-center px-6'>
                     <div
-                        class="
+                        class='
                             grid
                             grow
                             grid-rows-3 grid-cols-5
                             gap-x-12 gap-y-8
-                        "
+                        '
                     >
                         <div
-                            v-for="n in 15"
-                            class="
+                            v-for='n in 15'
+                            :key='n'
+                            class='
                                 flex-grow
                                 rounded-2xl
                                 bg-gray-50
                                 outline outline-1 outline-gray-300
                                 p-4
-                            "
+                            '
                         >
-                            <div class="flex-col p-2">
-                                <div class="overflow-ellipsis">
-                                    <div class="gap-4 flex justify-between">
-                                        <h1 class="text-black text-lg truncate">
+                            <div class='flex-col p-2'>
+                                <div class='overflow-ellipsis'>
+                                    <div class='gap-4 flex justify-between'>
+                                        <h1 class='text-black text-lg truncate'>
                                             Gel Polish
                                         </h1>
                                         <h1
-                                            class="
+                                            class='
                                                 text-orange-500 text-lg
                                                 truncate
-                                            "
+                                            '
                                         >
                                             $7.25
                                         </h1>
                                     </div>
                                     <p
-                                        class="
+                                        class='
                                             text-gray-500
                                             font-light
                                             truncate
-                                        "
+                                        '
                                     >
                                         Full Set
                                     </p>
                                 </div>
-                                <div class="mt-4 flex justify-center">
+                                <div class='mt-4 flex justify-center'>
                                     <button
-                                        class="
+                                        class='
                                             grow
                                             rounded-xl
                                             bg-gradient-to-r
@@ -73,7 +74,7 @@
                                             shadow-sm
                                             px-3
                                             py-3
-                                        "
+                                        '
                                     >
                                         Add to Cart
                                     </button>
@@ -82,9 +83,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="row-span-2 px-6 flex items-center">
+                <div class='row-span-2 px-6 flex items-center'>
                     <div
-                        class="
+                        class='
                             px-4
                             py-4
                             mb-4
@@ -92,30 +93,30 @@
                             justify-between
                             rounded-xl
                             shadow-lg
-                        "
+                        '
                     >
                         <UserPanel />
-                        <div class="bg-gray-100 w-0.5"></div>
+                        <div class='bg-gray-100 w-0.5'></div>
                     </div>
                 </div>
             </div>
         </template>
         <template #rightPanel>
             <div
-                class="
+                class='
                     grid
                     h-screen
                     grid-rows-12 grid-cols-1
                     bg-gray-100
                     shadow-inner
-                "
+                '
             >
-                <div class="row-span-1 flex justify-between items-center p-4">
-                    <hr2 class="text-3xl font-medium text-gray-900"
-                        >Current Order
+                <div class='row-span-1 flex justify-between items-center p-4'>
+                    <hr2 class='text-3xl font-medium text-gray-900'
+                    >Current Order
                     </hr2>
                     <button
-                        class="
+                        class='
                             block
                             rounded-xl
                             bg-red-400
@@ -124,18 +125,18 @@
                             px-5
                             py-2
                             font-body
-                        "
+                        '
                     >
                         Clear All
                     </button>
                 </div>
-                <div class="row-span-8">
+                <div class='row-span-8'>
                     <div>Cart viewport</div>
                 </div>
-                <div class="row-span-1">
+                <div class='row-span-1'>
                     <div>Cash/Credit Radio Group</div>
                 </div>
-                <div class="row-span-2">
+                <div class='row-span-2'>
                     <div>Confirmation Area</div>
                 </div>
             </div>
@@ -143,7 +144,7 @@
     </TwoPanelLayout>
 </template>
 
-<script lang="ts" setup>
+<script lang='ts' setup>
 import TwoPanelLayout from '@/layouts/TwoPanelLayout.vue';
 import UserPanel from '@/components/UserPanel.vue';
 import HealthIndicator from '@/components/HealthIndicator.vue';
