3a3871ddc9362ef985fbf9d3f90be15b32cba2f4
raytruong <raylintruong@gmail.com>
2022-01-28 16:37:47 -0600

    Add shadow to rounded buttons

--- src/components/shared/buttons/RoundedButton.vue
+++ src/components/shared/buttons/RoundedButton.vue
@@ -1,10 +1,11 @@
 <template>
     <button
-        :class="active ? 'bg-orange-400' : 'bg-gray-400'"
+        :class="props.active ? 'bg-orange-400' : 'bg-gray-400'"
         class="
             px-6
             py-3
             rounded-full
+            shadow-sm
             min-w-fit min-h-fit
             focus:brightness-110
             focus:outline-none
